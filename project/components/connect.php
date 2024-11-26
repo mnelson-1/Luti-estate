a3fb28adfc365693634ff34071ddec5d61b2b6d4
mnelson-1 <144142605+mnelson-1@users.noreply.github.com>
2024-11-27 01:40:38 +0200

    Update connect.php

--- project/components/connect.php
+++ project/components/connect.php
@@ -1,19 +1,20 @@
 <?php
 
-   $db_name = 'mysql:host=localhost;dbname=home_db';
-   $db_user_name = 'root';
-   $db_user_pass = '';
+      $server = "sql101.infinityfree.com"
+      $db_name = "if0_37794857_Luti_official";
+      $db_user_name = "if0_37794857";
+      $db_user_pass = "gdsiTewH39cO";
 
-   $conn = new PDO($db_name, $db_user_name, $db_user_pass);
+      $conn = new PDO($server, $db_name, $db_user_name, $db_user_pass);
 
-   function create_unique_id(){
+function create_unique_id(){
       $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
       $charactersLength = strlen($characters);
       $randomString = '';
-      for ($i = 0; $i < 20; $i++) {
-          $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
-      }
-      return $randomString;
-  }
+   for ($i = 0; $i < 20; $i++) {
+         $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+   }
+   return $randomString;
+}
 
 ?>
